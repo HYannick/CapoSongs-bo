fd75697ccc7c2fbaa8c75416a35fc5c6d78476e0
ayho <y.houssin01@gmail.com>
2023-01-12 11:13:52 +0100

    tests

--- config/admin.ts
+++ config/admin.ts
@@ -3,6 +3,6 @@ export default ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'r8Gp6BQWzx5WOuaBJqbKFw=='),
   },
 });
