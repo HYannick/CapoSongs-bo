d744bf30fa4fa0d320fca00899606efc87ceac96
Ynnk <y.houssin@live.fr>
2024-09-21 15:29:10 +0200

    upgrade strapi version

--- config/admin.ts
+++ config/admin.ts
@@ -5,4 +5,9 @@ export default ({ env }) => ({
   apiToken: {
     salt: env('API_TOKEN_SALT', 'r8Gp6BQWzx5WOuaBJqbKFw=='),
   },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT'),
+    },
+  },
 });
